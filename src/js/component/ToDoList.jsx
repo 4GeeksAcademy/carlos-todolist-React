e17daad35f71d6db3915f25654bd72e7a1f395fc
Carlos Alzate <158311146+cdalzate91@users.noreply.github.com>
2024-08-20 01:11:25 +0000

    Borar las tareas del servidor desde las x

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -7,14 +7,55 @@ const ToDoList = () => {
     const [newTask, setNewTask] = useState ("");
     
     const [taskList, setTaskList] = useState([]);
+
     
+    const createTask = async() => {
+        const updatedTasks = [...taskList, {label: newTask, is_done: false}];
+
+        const response = await fetch("https://playground.4geeks.com/todo/todos/cdalzate91", {
+            method: "POST",
+            body: JSON.stringify({
+                label: newTask,
+                is_done: false
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }        
+        })
+        if(response.ok){
+            loadTasks();
+            setNewTask('');
+        }
+    }
+    
+    const deleteTask = async(todoId, indice) => {
+        
+        const response = await fetch("https://playground.4geeks.com/todo/todos/" + todoId, {
+            method: "DELETE",
+        })
+        if(response.ok){
+            setTaskList(taskList.filter((_tarea, indiceBorrar)=>indice != indiceBorrar))
+        }
+    }
+
     const loadTasks = async() => {
-        const response = await fetch("https://playground.4geeks.com/todo/users/cdalzate")
-        const data = await response.json();
-        setTaskList(data.todos)
+        try {
+            const response = await fetch("https://playground.4geeks.com/todo/users/cdalzate91")
+            const data = await response.json();
+            setTaskList(data.todos)   
+        } catch (error) {
+            console.log(error);
+        }
     }   
 
+    const createUser = async() => {
+        const response = await fetch("https://playground.4geeks.com/todo/users/cdalzate91", {
+            method: "POST",
+       })
+    }
+
     useEffect(()=>{
+        createUser();
         loadTasks();
     } ,[])
 
@@ -26,18 +67,29 @@ const ToDoList = () => {
 
                     onKeyUp={(event) => {
                         if(event.key === "Enter"){
-                        setTaskList([newTask, ...taskList]);
-                            setNewTask('');  
+                        createTask();
                         }
                     }}
                     className="full-width-input"
                 />
                     {(taskList.length == 0) && <div className="noTasks">No tasks, add a task</div> }
-                    {taskList.map((tarea, indice)=> (<Task task={tarea} key={indice} onRemove={()=>{
-                    setTaskList(taskList.filter((_tarea, indiceBorrar)=>indice != indiceBorrar))
+                    {taskList.map((tarea, indice) => (
+                        <Task 
+                            task={tarea} 
+                            key={indice} 
+                            onRemove={()=>{
+                                deleteTask(tarea.id, indice)
                     }}/>))}
                 <p className="contadorTareas text-start">{taskList.length} item left</p>
+                
+
+                {/* Agregar código de eliminar tarea del servidor */}
+                <button className="btn btn-success" onClick={() => setTaskList([])}>
+                    Clear    
+                </button>
+            
             </div>
+            
              
         
     );
